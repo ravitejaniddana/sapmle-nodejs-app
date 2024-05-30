d9457618e6b02613590029ad0f819422f78b3f87
Raviteja <lovelyravitej855@gmail.com>
2024-05-30 18:18:05 +0530

    add all files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+const port  = 5000;
+const app = express();
+
+app.get("/", function(req, res){
+    res.send(" hai hello world");
+});
+
+app.listen(port, function(){
+    console.log("listening in port number 5000");
+});
